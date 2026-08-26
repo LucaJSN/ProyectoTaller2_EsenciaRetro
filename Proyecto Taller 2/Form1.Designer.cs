6e5ae5e1fbb479b20d429616f8cab420e34fc99a
Luca <lucajoaquinnsn@gmail.com>
2026-08-26 14:50:19 -0300

    Estructura base de menú lateral y navegación por UserControls

--- Proyecto Taller 2/Form1.Designer.cs
+++ Proyecto Taller 2/Form1.Designer.cs
@@ -28,13 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panelContenedor = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnInicio = new System.Windows.Forms.Button();
+            this.btnProductos = new System.Windows.Forms.Button();
+            this.btnVentas = new System.Windows.Forms.Button();
+            this.btnBalance = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelContenedor
+            // 
+            this.panelContenedor.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panelContenedor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContenedor.Location = new System.Drawing.Point(200, 0);
+            this.panelContenedor.Name = "panelContenedor";
+            this.panelContenedor.Size = new System.Drawing.Size(1053, 445);
+            this.panelContenedor.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Controls.Add(this.btnVentas);
+            this.panel1.Controls.Add(this.btnBalance);
+            this.panel1.Controls.Add(this.btnInicio);
+            this.panel1.Controls.Add(this.btnProductos);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 445);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(200, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1053, 60);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnInicio
+            // 
+            this.btnInicio.Location = new System.Drawing.Point(62, 95);
+            this.btnInicio.Name = "btnInicio";
+            this.btnInicio.Size = new System.Drawing.Size(75, 23);
+            this.btnInicio.TabIndex = 0;
+            this.btnInicio.Text = "Inicio";
+            this.btnInicio.UseVisualStyleBackColor = true;
+            this.btnInicio.Click += new System.EventHandler(this.btnInicio_Click);
+            // 
+            // btnProductos
+            // 
+            this.btnProductos.Location = new System.Drawing.Point(62, 150);
+            this.btnProductos.Name = "btnProductos";
+            this.btnProductos.Size = new System.Drawing.Size(75, 23);
+            this.btnProductos.TabIndex = 1;
+            this.btnProductos.Text = "Productos y Stock";
+            this.btnProductos.UseVisualStyleBackColor = true;
+            this.btnProductos.Click += new System.EventHandler(this.btnProductos_Click);
+            // 
+            // btnVentas
+            // 
+            this.btnVentas.Location = new System.Drawing.Point(16, 210);
+            this.btnVentas.Name = "btnVentas";
+            this.btnVentas.Size = new System.Drawing.Size(178, 23);
+            this.btnVentas.TabIndex = 2;
+            this.btnVentas.Text = "Ventas y Stock";
+            this.btnVentas.UseVisualStyleBackColor = true;
+            this.btnVentas.Click += new System.EventHandler(this.btnVentas_Click);
+            // 
+            // btnBalance
+            // 
+            this.btnBalance.Location = new System.Drawing.Point(62, 264);
+            this.btnBalance.Name = "btnBalance";
+            this.btnBalance.Size = new System.Drawing.Size(75, 23);
+            this.btnBalance.TabIndex = 3;
+            this.btnBalance.Text = "Balance Económico";
+            this.btnBalance.UseVisualStyleBackColor = true;
+            this.btnBalance.Click += new System.EventHandler(this.btnBalance_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1253, 445);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panelContenedor);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelContenedor;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnInicio;
+        private System.Windows.Forms.Button btnVentas;
+        private System.Windows.Forms.Button btnProductos;
+        private System.Windows.Forms.Button btnBalance;
     }
 }
 
--- Proyecto Taller 2/Form1.cs
+++ Proyecto Taller 2/Form1.cs
@@ -7,14 +7,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace Proyecto_Taller_2
 {
     public partial class Form1 : Form
     {
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Código a ejecutar cuando la ventana termine de cargar
+        }
         public Form1()
         {
             InitializeComponent();
+            // Carga la pantalla de inicio al abrir el programa
+            AbrirModulo(new UC_Dashboard());
+        }
+
+        // Método encargado de cambiar la vista dentro del panel contenedor
+        private void AbrirModulo(UserControl modulo)
+        {
+            panelContenedor.Controls.Clear();
+            modulo.Dock = DockStyle.Fill;
+            panelContenedor.Controls.Add(modulo);
+            panelContenedor.Tag = modulo;
+            modulo.BringToFront();
+        }
+
+        private void btnInicio_Click(object sender, EventArgs e)
+        {
+            AbrirModulo(new UC_Dashboard());
+        }
+
+        private void btnBalance_Click(object sender, EventArgs e)
+        {
+            AbrirModulo(new UC_Balance());
+        }
+
+        private void btnProductos_Click(object sender, EventArgs e)
+        {
+            AbrirModulo(new UC_Productos());
+        }
+
+        private void btnVentas_Click(object sender, EventArgs e)
+        {
+            AbrirModulo(new UC_Ventas());
         }
     }
 }
