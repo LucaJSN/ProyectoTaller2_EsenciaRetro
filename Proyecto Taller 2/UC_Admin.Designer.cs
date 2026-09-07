2ef3c3a8fb13edb81f899324a4a2fc9f7aee8afd
LucaJSN <lucajoaquinnsn@gmail.com>
2026-09-07 18:45:09 -0300

    Campos de CRUD Usuarios VALIDADOS

--- Proyecto Taller 2/UC_Admin.Designer.cs
+++ Proyecto Taller 2/UC_Admin.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.DGVUsuarios = new System.Windows.Forms.DataGridView();
             this.CID = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -38,13 +39,41 @@
             this.CTelefono = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.CActivo = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.CAcciones = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.TBNombre = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.TBApellido = new System.Windows.Forms.TextBox();
+            this.TBTelefono = new System.Windows.Forms.TextBox();
+            this.TBCorreo = new System.Windows.Forms.TextBox();
+            this.TBContraseña = new System.Windows.Forms.TextBox();
+            this.CBRol = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.TBCiudad = new System.Windows.Forms.TextBox();
+            this.TBAltura = new System.Windows.Forms.TextBox();
+            this.TBCalle = new System.Windows.Forms.TextBox();
+            this.TBProvincia = new System.Windows.Forms.TextBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.BtnAgregar = new System.Windows.Forms.Button();
+            this.BtnCancelar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.DGVUsuarios)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(285, 136);
+            this.label1.Location = new System.Drawing.Point(285, 173);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(0, 16);
             this.label1.TabIndex = 0;
@@ -125,14 +154,301 @@
             this.CAcciones.Name = "CAcciones";
             this.CAcciones.Width = 125;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(298, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(458, 39);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Administración de Usuarios";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(166, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Crear un Usuario:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Nombre";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 173);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Apellido";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(302, 119);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 16);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Correo";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(302, 170);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(76, 16);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Contraseña";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(15, 225);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(61, 16);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Telefono";
+            // 
+            // TBNombre
+            // 
+            this.TBNombre.Location = new System.Drawing.Point(123, 119);
+            this.TBNombre.Name = "TBNombre";
+            this.TBNombre.Size = new System.Drawing.Size(100, 22);
+            this.TBNombre.TabIndex = 9;
+            this.TBNombre.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // TBApellido
+            // 
+            this.TBApellido.Location = new System.Drawing.Point(123, 170);
+            this.TBApellido.Name = "TBApellido";
+            this.TBApellido.Size = new System.Drawing.Size(100, 22);
+            this.TBApellido.TabIndex = 11;
+            this.TBApellido.TextChanged += new System.EventHandler(this.TBApellido_TextChanged);
+            // 
+            // TBTelefono
+            // 
+            this.TBTelefono.Location = new System.Drawing.Point(127, 222);
+            this.TBTelefono.Name = "TBTelefono";
+            this.TBTelefono.Size = new System.Drawing.Size(100, 22);
+            this.TBTelefono.TabIndex = 12;
+            this.TBTelefono.TextChanged += new System.EventHandler(this.TBTelefono_TextChanged);
+            // 
+            // TBCorreo
+            // 
+            this.TBCorreo.Location = new System.Drawing.Point(416, 119);
+            this.TBCorreo.Name = "TBCorreo";
+            this.TBCorreo.Size = new System.Drawing.Size(126, 22);
+            this.TBCorreo.TabIndex = 13;
+            this.TBCorreo.TextChanged += new System.EventHandler(this.TBCorreo_TextChanged);
+            // 
+            // TBContraseña
+            // 
+            this.TBContraseña.Location = new System.Drawing.Point(416, 167);
+            this.TBContraseña.Name = "TBContraseña";
+            this.TBContraseña.PasswordChar = '*';
+            this.TBContraseña.Size = new System.Drawing.Size(126, 22);
+            this.TBContraseña.TabIndex = 14;
+            this.TBContraseña.UseSystemPasswordChar = true;
+            // 
+            // CBRol
+            // 
+            this.CBRol.FormattingEnabled = true;
+            this.CBRol.Items.AddRange(new object[] {
+            "Vendedor",
+            "Supervisor",
+            "Administrador"});
+            this.CBRol.Location = new System.Drawing.Point(416, 214);
+            this.CBRol.Name = "CBRol";
+            this.CBRol.Size = new System.Drawing.Size(121, 24);
+            this.CBRol.TabIndex = 15;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(302, 222);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(28, 16);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Rol";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(842, 78);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(99, 25);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "Dirección:";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(636, 122);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(63, 16);
+            this.label11.TabIndex = 18;
+            this.label11.Text = "Provincia";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(636, 173);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(50, 16);
+            this.label12.TabIndex = 19;
+            this.label12.Text = "Ciudad";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(903, 127);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(38, 16);
+            this.label13.TabIndex = 20;
+            this.label13.Text = "Calle";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(903, 178);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(41, 16);
+            this.label14.TabIndex = 21;
+            this.label14.Text = "Altura";
+            // 
+            // TBCiudad
+            // 
+            this.TBCiudad.Location = new System.Drawing.Point(715, 167);
+            this.TBCiudad.Name = "TBCiudad";
+            this.TBCiudad.Size = new System.Drawing.Size(126, 22);
+            this.TBCiudad.TabIndex = 22;
+            this.TBCiudad.TextChanged += new System.EventHandler(this.TBCiudad_TextChanged);
+            // 
+            // TBAltura
+            // 
+            this.TBAltura.Location = new System.Drawing.Point(967, 170);
+            this.TBAltura.Name = "TBAltura";
+            this.TBAltura.Size = new System.Drawing.Size(126, 22);
+            this.TBAltura.TabIndex = 23;
+            this.TBAltura.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // TBCalle
+            // 
+            this.TBCalle.Location = new System.Drawing.Point(967, 127);
+            this.TBCalle.Name = "TBCalle";
+            this.TBCalle.Size = new System.Drawing.Size(126, 22);
+            this.TBCalle.TabIndex = 24;
+            this.TBCalle.TextChanged += new System.EventHandler(this.TBCalle_TextChanged);
+            // 
+            // TBProvincia
+            // 
+            this.TBProvincia.Location = new System.Drawing.Point(715, 122);
+            this.TBProvincia.Name = "TBProvincia";
+            this.TBProvincia.Size = new System.Drawing.Size(126, 22);
+            this.TBProvincia.TabIndex = 25;
+            this.TBProvincia.TextChanged += new System.EventHandler(this.TBProvincia_TextChanged);
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(51, 78);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(95, 25);
+            this.label15.TabIndex = 26;
+            this.label15.Text = "Personal:";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label16.Location = new System.Drawing.Point(357, 78);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(82, 25);
+            this.label16.TabIndex = 27;
+            this.label16.Text = "Cuenta:";
+            // 
+            // BtnAgregar
+            // 
+            this.BtnAgregar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.BtnAgregar.BackColor = System.Drawing.Color.Green;
+            this.BtnAgregar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnAgregar.Font = new System.Drawing.Font("Microsoft YaHei", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnAgregar.Location = new System.Drawing.Point(305, 313);
+            this.BtnAgregar.Name = "BtnAgregar";
+            this.BtnAgregar.Size = new System.Drawing.Size(145, 76);
+            this.BtnAgregar.TabIndex = 28;
+            this.BtnAgregar.Text = "Agregar";
+            this.BtnAgregar.UseVisualStyleBackColor = false;
+            this.BtnAgregar.Click += new System.EventHandler(this.BtnAgregar_Click);
+            // 
+            // BtnCancelar
+            // 
+            this.BtnCancelar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.BtnCancelar.BackColor = System.Drawing.Color.Red;
+            this.BtnCancelar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnCancelar.Font = new System.Drawing.Font("Microsoft YaHei", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCancelar.Location = new System.Drawing.Point(611, 313);
+            this.BtnCancelar.Name = "BtnCancelar";
+            this.BtnCancelar.Size = new System.Drawing.Size(145, 76);
+            this.BtnCancelar.TabIndex = 29;
+            this.BtnCancelar.Text = "Cancelar";
+            this.BtnCancelar.UseVisualStyleBackColor = false;
+            // 
             // UC_Admin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.BtnCancelar);
+            this.Controls.Add(this.BtnAgregar);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.TBProvincia);
+            this.Controls.Add(this.TBCalle);
+            this.Controls.Add(this.TBAltura);
+            this.Controls.Add(this.TBCiudad);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.CBRol);
+            this.Controls.Add(this.TBContraseña);
+            this.Controls.Add(this.TBCorreo);
+            this.Controls.Add(this.TBTelefono);
+            this.Controls.Add(this.TBApellido);
+            this.Controls.Add(this.TBNombre);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.DGVUsuarios);
             this.Controls.Add(this.label1);
             this.Name = "UC_Admin";
-            this.Size = new System.Drawing.Size(1225, 796);
+            this.Size = new System.Drawing.Size(1244, 796);
+            this.Load += new System.EventHandler(this.UC_Admin_Load);
             ((System.ComponentModel.ISupportInitialize)(this.DGVUsuarios)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -151,5 +467,33 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn CTelefono;
         private System.Windows.Forms.DataGridViewTextBoxColumn CActivo;
         private System.Windows.Forms.DataGridViewTextBoxColumn CAcciones;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox TBNombre;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox TBApellido;
+        private System.Windows.Forms.TextBox TBTelefono;
+        private System.Windows.Forms.TextBox TBCorreo;
+        private System.Windows.Forms.TextBox TBContraseña;
+        private System.Windows.Forms.ComboBox CBRol;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox TBCiudad;
+        private System.Windows.Forms.TextBox TBAltura;
+        private System.Windows.Forms.TextBox TBCalle;
+        private System.Windows.Forms.TextBox TBProvincia;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Button BtnAgregar;
+        private System.Windows.Forms.Button BtnCancelar;
     }
 }
--- Proyecto Taller 2/UC_Admin.cs
+++ Proyecto Taller 2/UC_Admin.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySqlConnector;
+using System.Text.RegularExpressions; //Para validar correo
 
 namespace Proyecto_Taller_2
 {
@@ -21,6 +22,183 @@ namespace Proyecto_Taller_2
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
             
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TBNombre_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Permitir solo letras, la tecla de borrado (BackSpace) y la barra espaciadora
+            if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar))
+            {
+                e.Handled = true; // Bloquea el caracter
+                return;
+            }
+
+            // Lógica para permitir solo 1 espacio (máximo 2 palabras)
+            if (char.IsWhiteSpace(e.KeyChar))
+            {
+                TextBox txt = sender as TextBox;
+
+                // Si ya hay un espacio en el texto, o si es el primer caracter, bloqueamos el nuevo espacio
+                if (txt.Text.Contains(" ") || txt.Text.Length == 0)
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void UC_Admin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TBAltura_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Permitir solo números y la tecla de borrado
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true; // Bloquea letras y símbolos
+            }
+        }
+
+        private void TBCalle_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Permitir solo letras, la tecla de borrado (BackSpace) y la barra espaciadora
+            if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar))
+            {
+                e.Handled = true; // Bloquea el caracter
+                return;
+            }
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnAgregar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TBApellido_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TBApellido_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Permitir solo letras, la tecla de borrado (BackSpace) y la barra espaciadora
+            if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar))
+            {
+                e.Handled = true; // Bloquea el caracter
+                return;
+            }
+
+            // Lógica para permitir solo 1 espacio (máximo 2 palabras)
+            if (char.IsWhiteSpace(e.KeyChar))
+            {
+                TextBox txt = sender as TextBox;
+
+                // Si ya hay un espacio en el texto, o si es el primer caracter, bloqueamos el nuevo espacio
+                if (txt.Text.Contains(" ") || txt.Text.Length == 0)
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void TBTelefono_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TBTelefono_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Permitir solo números y la tecla de borrado
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true; // Bloquea letras y símbolos
+            }
+        }
+
+        private void TBCorreo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TBCorreo_Leave(object sender, EventArgs e)
+        {
+            // Si está vacío, no hacemos la validación (puedes requerirlo en el botón Guardar)
+            if (string.IsNullOrWhiteSpace(TBCorreo.Text)) return;
+
+            // Patrón Regex para validar formato@dominio.algo
+            string patron = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+
+            if (!Regex.IsMatch(TBCorreo.Text, patron))
+            {
+                MessageBox.Show("El formato del correo es inválido. Debe ser ejemplo@dominio.com",
+                                "Correo Inválido",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+
+                TBCorreo.Focus(); // Opcional: Mantiene el cursor en el TextBox hasta que lo corrija
+            }
+        }
+
+        private void TBProvincia_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private void TBProvincia_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Permitir solo letras, la tecla de borrado (BackSpace) y la barra espaciadora
+            if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar))
+            {
+                e.Handled = true; // Bloquea el caracter
+                return;
+            }
+
+            // Lógica para permitir solo 1 espacio (máximo 2 palabras)
+            if (char.IsWhiteSpace(e.KeyChar))
+            {
+                TextBox txt = sender as TextBox;
+
+                // Si ya hay un espacio en el texto, o si es el primer caracter, bloqueamos el nuevo espacio
+                if (txt.Text.Contains(" ") || txt.Text.Length == 0)
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void TBCiudad_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TBCiudad_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Permitir solo letras, la tecla de borrado (BackSpace) y la barra espaciadora
+            if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar))
+            {
+                e.Handled = true; // Bloquea el caracter
+                return;
+            }
+        }
+
+        private void TBCalle_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
